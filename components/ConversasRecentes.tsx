979ae2080fb86a13f20d9078703b54ac1b6b26dd
KauaSd <kauas657@gmail.com>
2026-09-13 16:14:40 -0300

    componentes conversasrecentes e historico de conversas

---
+++ components/ConversasRecentes.tsx
@@ -0,0 +1,44 @@
+import TagNeuro from "./TagNeuro";
+import { pegainicial } from "@/utils/pegariniciais";
+interface RecentesProps {
+  aluno: string;
+  Neuro: string;
+  data: Date;
+  turma: string;
+  chat: string;
+}
+
+function formatarData(data: Date): string {
+  return new Intl.DateTimeFormat("pt-BR", {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  })
+    .format(data)
+    .replace(/ de /g, " ");
+}
+
+export default function ConversasRecentes(props: RecentesProps) {
+  return (
+    <div className="flex px-6 items-center justify-between w-full h-22! sm:h-25! md:h-27.5! bg-[#FFFDFA] rounded-[70px] text-[#433F3F]">
+      <div className="flex gap-3 w-full">
+        <div className="flex shrink-0 items-center justify-center w-14 h-14 rounded-full bg-[#FF9999] text-2xl font-extrabold">
+          {pegainicial(props.aluno)}
+        </div>
+        <div className="flex flex-col gap-3 w-full">
+        <div className="flex justify-between w-full">
+          <a className="text-[16px] font-bold">
+            {props.aluno} - {props.chat}
+          </a>
+          <a className="text-sm mr-0! sm:mr-12! md:mr-17!  "> {formatarData(props.data)} </a>
+          </div>
+          <div className="flex gap-2">
+            <TagNeuro label={props.Neuro} />
+            <a className="text-sm">{props.turma}</a>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
---
+++ components/HistoricoTags.tsx
@@ -0,0 +1,38 @@
+interface HistoricoProps {
+  data: Date;
+  chat: string;
+}
+
+function formatarData(data: Date): string {
+  return new Intl.DateTimeFormat("pt-BR", {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  })
+    .format(data)
+    .replace(/ de /g, " ");
+}
+
+export default function ConversasRecentes(props: HistoricoProps) {
+  return (
+    <div className="flex px-6 items-center gap-4 w-full h-20! sm:h-22! md:h-25.25! bg-[#FFFDFA] rounded-[70px]">
+    <div className="flex shrink-0 items-center justify-center w-14 h-14 rounded-full bg-[#433F3F] text-2xl font-extrabold">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_394_535)">
+<path d="M20 2H4C2.9 2 2 2.9 2 4V22L6 18H20C21.1 18 22 17.1 22 16V4C22 2.9 21.1 2 20 2ZM9 11H7V9H9V11ZM13 11H11V9H13V11ZM17 11H15V9H17V11Z" fill="#FFFDFA"/>
+</g>
+<defs>
+<clipPath id="clip0_394_535">
+<rect width="24" height="24" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+
+    </div>
+    <div className="flex flex-col ">
+        <a className="text-[18px] text-[#433F3F]">{props.chat}</a>
+          <a className="text-sm mr-0! sm:mr-12! md:mr-17! text-[#797979]"> {formatarData(props.data)} </a>
+          </div>
+    </div>
+  );
+}
